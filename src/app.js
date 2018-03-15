47499c5caad5d1a7995e330b37929461a1db1a5b
Rodrigo Bustamante <rodrigo@ormasoft.cl>
2018-03-15 14:45:13 -0300

    Added navbar base

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ export class App {
   constructor() {
     this.message = 'Hello World!';
   }
+
+  configureRouter(config, router){
+    config.map([
+      { route: '', name: 'Inicio', moduleId: './pantallas/welcome/index', title: 'Inicio' }
+    ])
+  }
 }
--- src/pantallas/welcome/index.js
+++ src/pantallas/welcome/index.js
@@ -1,5 +1,17 @@
-export class Welcome {
-  constructor() {
+import { inject } from 'aurelia-framework';
+import { HttpClient, json } from 'aurelia-fetch-client';
 
+@inject(HttpClient)
+export class Welcome {
+  constructor(HttpClient) {
+    this.httpClient = HttpClient;
+    this.url = "http://jsonplaceholder.typicode.com/posts/1";
+  }
+  getApi() {
+    this.httpClient.fetch(this.url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
   }
 }
